3dc9b07f32842145c8a35f262bd35ed579643640
Данил Посохов <Iamposox@yandex.ru>
2021-02-21 02:47:01 +0300

    rename var point => var count in Program.Main

--- Polygon/Program.cs
+++ Polygon/Program.cs
@@ -21,14 +21,14 @@ namespace Polygon {
                 var address = ReadLine();
 
                 Write("Введите частоту точек: ");
-                int.TryParse(ReadLine(), out var point);
+                int.TryParse(ReadLine(), out var count);
 
                 Write("Введите имя файла для сохранения: ");
                 var fileName = ReadLine();
 
                 var polygon = await osmService.GetPolygon(address);
 
-                await fileManager.WriteFile(polygon, fileName.Contains(".txt") ? fileName : fileName + ".txt", point > 0 ? point : 1);
+                await fileManager.WriteFile(polygon, fileName.Contains(".txt") ? fileName : fileName + ".txt", count > 0 ? count : 1);
             } catch (Exception ex) {
                 _logger.Error(ex);
                 throw;
